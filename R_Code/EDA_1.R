05ef108777231ec042b529444bd8108b18f99e38
Bruce Granger <brucegranger@gmail.com>
2018-11-11 19:16:40 -0600

    Updated Factor Ordering
    Updated the ordering of the ordering of the factors in the Boxplot

--- R_Code/EDA_1.R
+++ R_Code/EDA_1.R
@@ -38,12 +38,18 @@ as_train_cont_tall <- as_train_cont %>%
   gather(cont1:cont14, key = "Variable", value = "Value") %>%
   arrange(Variable)
 
+as_train_cont_tall$Variable <- as.factor(as_train_cont_tall$Variable)
 
+
+as_train_cont_tall$Variable <- factor(as_train_cont_tall$Variable, 
+                                      levels = c("cont1", "cont2", "cont3", "cont4", "cont5",
+                                                 "cont6", "cont7", "cont8", "cont9", "cont10",
+                                                 "cont11", "cont12", "cont13", "cont14"))
 # PLOTTING ####
 filepath = "/Users/bgranger/Documents/SMU/6372_Applied Statistics/MSDS6372/Applied_Stats_Project_2/Proj2_Doc/6372_proj2/R_Output"
 
 ggplot(data = as_train_cont_tall, aes(x=Variable, y=Value)) + 
   geom_boxplot() +
-  ggtitle("BP1: Continious Variables 1 through 14") 
+  ggtitle("BP1: Continuous Variables 1 through 14") 
 ggsave("BP1.png", plot = last_plot(), path = filepath, width = 50, height = 20,
        units = "cm")
